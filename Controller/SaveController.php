cbc5101c13e3d6d9325b1d5fdd9654281b5beea9
Ivo Azirjans <ivo.azirjans@vvsoftmedia.com>
2014-04-24 13:57:25 +0300

    Fixed missing JsonResponse class use definition

--- Controller/SaveController.php
+++ Controller/SaveController.php
@@ -4,6 +4,7 @@ namespace Arcana\Bundle\ContentBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
